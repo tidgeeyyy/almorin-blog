01fb02535599c1510713919ae3509be6264dc81a
tidgeeyyy <thomasjosephalmorin123@gmail.com>
2024-11-09 01:25:45 +0800

    update

--- src/script.js
+++
@@ -1,6 +0,0 @@
-// Get the theme toggle button
-const themeToggleBtn = document.getElementById('theme-toggle');
-
-// Check if a theme is saved in localStorage, and apply it
-if (localStorage.getItem('theme') === 'dark') {
-    document
